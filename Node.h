ba9c743f2747e59d4236300b78f25bd495fa3523
easybarezi20 <easybarezi20@gmail.com>
2026-03-01 18:24:48 -0800

    fixed main.cpp file and added a Node.h file

---
+++ Node.h
@@ -0,0 +1,23 @@
+#ifndef NODE_H
+#define NODE_H
+class Node 
+{
+public:
+    //constructors
+    Node() : data(0), next(nullptr){}
+    Node(int newData, Node* newNext) : data(newData), next(newNext) {}
+    //setters
+    void setData(int newData){ data = newData; }
+    void setNext(Node* newNext){ next = newNext; }
+
+    //getters
+    int getData() const { return data; }
+    Node* getNext() const { return next; }
+
+    ~Node() {}
+
+private:
+    int data;
+    Node* next;
+};
+#endif
